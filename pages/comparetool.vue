57a475f22b01881c418da7690d6c348582902b6b
Aidan Hibbard <aidanchibbard@gmail.com>
2021-08-10 11:06:08 -0700

    I think that's it for the compare tool functionality

--- pages/comparetool.vue
+++ pages/comparetool.vue
@@ -7,6 +7,21 @@
                 v-model="UrlOne"
                 outlined
             ></v-text-field>
+            <v-checkbox
+                v-model="CboxOne"
+                :label="`GraphQl ${CboxOne.toString()}`"
+            ></v-checkbox>
+            <p>Or enter a payload you'd like to compare <br /> (note: For graphQL requests please paste the request in here and select GraphQL)</p>
+            <v-textarea
+                outlined
+                clearable
+                clear-icon="mdi-close-circle"
+                label="Box One"
+                value=""
+                v-model="this.TextAreaOne"
+            ></v-textarea>
+            <v-spacer />
+            <hr />
             <v-spacer />
             <v-text-field
                 label="URL Two"
@@ -14,6 +29,19 @@
                 v-model="UrlTwo"
                 outlined
             ></v-text-field>
+            <v-checkbox
+                v-model="CboxTwo"
+                :label="`GraphQl ${CboxTwo.toString()}`"
+            ></v-checkbox>
+            <p>Or enter a payload you'd like to compare <br /> (note: For graphQL requests please paste the request in here and select GraphQL)</p>
+                <v-textarea
+                outlined
+                clearable
+                clear-icon="mdi-close-circle"
+                label="Box Two"
+                value=""
+                v-model="this.TextAreatwo"
+            ></v-textarea>
                 <v-btn
                 @click="this.FetchAndCompare"
                 >
@@ -37,18 +65,45 @@ export default {
             PayloadDiff: null,
             UrlOne: null,
             UrlTwo: null,
+            CboxOne: false,
+            CboxTwo: false,
+            TextAreaOne: null,
+            TextAreaTwo: null,
         }
     },
     methods: {
         async FetchAndCompare() {
-            const a = await this.$axios.$get(
-                this.UrlOne
-            )
-            const b = await this.$axios.$get(
-                this.UrlTwo
-            )
+            let a, b;
+            if (this.CboxOne === true) {
+                // Gql req
+                a = await this.$axios.$get({
+                    url: `${this.UrlOne}`,
+                    method: 'post',
+                    data: {
+                        query: `${this.TextAreaOne}`, // Add in text field value
+                    }
+                })
+            } else {
+                // Check for payload in text area then go for regular req
+                if (this.TextAreaOne != null) a = this.TextAreaOne
+                a = await this.$axios.$get(`${this.UrlOne}`)
+            }
+            if (this.CboxTwo === true) {
+                // gql req
+                b = await this.$axios.$get({
+                    url: `${this.UrlTwo}`,
+                    method: 'post',
+                    data: {
+                        query: `${this.TextAreaTwo}`, // Add in text field value
+                    }
+                })
+            } else {
+                // Check for payload in text area then go for regular req
+                if (this.TextAreaTwo != null) b = this.TextAreaTwo
+                b = await this.$axios.$get(`${this.UrlTwo}`)
+            } 
             this.PayloadDiff = diff(a,b)
-        },
+        }
     },
 }
 </script>
